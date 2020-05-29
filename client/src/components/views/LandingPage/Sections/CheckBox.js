6d0d8e997f073775757711c8bdc9937e8fea25bb
unknown <khanhigoufake2@gmail.com>
2020-05-30 00:04:07 +0200

    console.log

--- client/src/components/views/LandingPage/Sections/CheckBox.js
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -24,6 +24,7 @@ const useStyles = makeStyles(() => ({
 }));
 
 function CheckBox(props) {
+  console.log("Landing");
   const classes = useStyles();
   const [Checked, setChecked] = useState([]);
   const [hidden, sethidden] = useState(true);
